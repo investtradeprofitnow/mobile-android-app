fb0ba39166dd730556a69af66343d5eef036ba2f
Invest TRade Profit Now <sddmsinvesttradeprofitnow@gmail.com>
2023-07-11 16:31:04 +0530

    Strategy List Updated With Filter View

--- app/src/main/java/com/itpn/activity/StrategyListActivity.java
+++ app/src/main/java/com/itpn/activity/StrategyListActivity.java
@@ -9,6 +9,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -24,10 +29,17 @@ import org.json.JSONObject;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 public class StrategyListActivity extends MenuForActivity implements AppConfig {
 	ArrayList<Strategy> strategyList;
+	ArrayList<Strategy> intradayList;
+	ArrayList<Strategy> btstList;
+	ArrayList<Strategy> positionalList;
+	ArrayList<Strategy> investmentList;
 	RecyclerView recyclerView;
+	Spinner filter;
+	TextView emptyStrategyList;
 	StrategyShortAdapter adapter;
 	Context context;
 
@@ -36,8 +48,22 @@ public class StrategyListActivity extends MenuForActivity implements AppConfig {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_strategy_list);
 		recyclerView = findViewById(R.id.rv_strategy_list);
+		filter = findViewById(R.id.sp_filter);
+		emptyStrategyList = findViewById(R.id.tv_empty_list);
+		List<String> categories = new ArrayList<String>();
+		categories.add("All");
+		categories.add("Intraday");
+		categories.add("BTST");
+		categories.add("Positional");
+		categories.add("Investment");
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,categories);
+		filter.setAdapter(dataAdapter);
 		context = this;
 		strategyList = new ArrayList<>();
+		intradayList = new ArrayList<>();
+		btstList = new ArrayList<>();
+		positionalList = new ArrayList<>();
+		investmentList = new ArrayList<>();
 		Intent intent = getIntent();
 		String pageName = intent.getStringExtra("Page");
 		if("".equals(pageName)){
@@ -51,10 +77,59 @@ public class StrategyListActivity extends MenuForActivity implements AppConfig {
 			recyclerView.setLayoutManager(layoutManager);
 			recyclerView.setAdapter(adapter);
 		}
-		else{
+		else {
 			GetStrategyList getStrategyList = new GetStrategyList();
 			getStrategyList.execute();
 		}
+		filter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+				String item = parent.getItemAtPosition(position).toString();
+				boolean isEmptyList = false;
+				emptyStrategyList.setVisibility(View.GONE);
+				switch (item){
+					case "Intraday":
+						if(intradayList.size()==0){
+							isEmptyList=true;
+						}
+						adapter = new StrategyShortAdapter(context,intradayList);
+						break;
+					case "BTST":
+						if(btstList.size()==0){
+							isEmptyList=true;
+						}
+						adapter = new StrategyShortAdapter(context,btstList);
+						break;
+					case "Positional":
+						if(positionalList.size()==0){
+							isEmptyList=true;
+						}
+						adapter = new StrategyShortAdapter(context,positionalList);
+						break;
+					case "Investment":
+						if(investmentList.size()==0){
+							isEmptyList=true;
+						}
+						adapter = new StrategyShortAdapter(context,investmentList);
+						break;
+					case "All":
+						adapter = new StrategyShortAdapter(context,strategyList);
+						break;
+				}
+
+				if(isEmptyList){
+					emptyStrategyList.setVisibility(View.VISIBLE);
+				}
+				RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false);
+				recyclerView.setLayoutManager(layoutManager);
+				recyclerView.setAdapter(adapter);
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+
+			}
+		});
 	}
 
 	@Override
@@ -89,6 +164,20 @@ public class StrategyListActivity extends MenuForActivity implements AppConfig {
 					JSONObject strategyJson = jsonArray.getJSONObject(i);
 					Strategy strategy = new Strategy(strategyJson);
 					strategyList.add(strategy);
+					switch (strategy.getType()){
+						case "Intraday":
+							intradayList.add(strategy);
+							break;
+						case "BTST":
+							btstList.add(strategy);
+							break;
+						case "Positional":
+							positionalList.add(strategy);
+							break;
+						case "Investment":
+							investmentList.add(strategy);
+							break;
+					}
 				}
 				SharedPreferences sharedPreferences = getSharedPreferences("ITPN",MODE_PRIVATE);
 				SharedPreferences.Editor editor = sharedPreferences.edit();
